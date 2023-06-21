371b01f9bf2ae45434afdada24c007d0af78fb3d
prashant hansda <2020eeb1192@iitrpr.ac.in>
2023-06-21 21:33:33 +0530

    updated

--- client/src/Pages/Questions/QuestionsDetails.jsx
+++ client/src/Pages/Questions/QuestionsDetails.jsx
@@ -82,7 +82,7 @@ const QuestionsDetails = () => {
    const Navigate = useNavigate()
    const dispatch = useDispatch()
    const location =useLocation()
-   const url = 'http://localhost:3000/'
+   const url = 'https://stack-overflow-qfdp.onrender.com'
 
 
    const User = useSelector((state) => state.currentUserReducer)
--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const API = axios.create({ baseURL: 'http://localhost:5000'})
+const API = axios.create({ baseURL: 'https://stack-overflow-qfdp.onrender.com'})
 
 
 API.interceptors.request.use((req) => {
