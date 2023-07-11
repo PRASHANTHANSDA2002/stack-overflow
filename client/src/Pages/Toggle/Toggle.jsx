bbf89a7aeb4ff5c24b4e402cff0a357534f0de9c
PRASHANTHANSDA2002 <2020eeb1192@iitrpr.ac.in>
2023-07-11 22:00:45 +0530

    finally

--- client/src/Pages/Toggle/Toggle.jsx
+++ client/src/Pages/Toggle/Toggle.jsx
@@ -6,9 +6,9 @@ const Toggle = () => {
     <Link to="/" className="jety">Home</Link>
     <Link to="/Questions" className="jety">Questions </Link>
     <Link to="/Tags" className="jety">Tags</Link>
-    <Link to="/Users" className="jety">Users </Link>
+    {/* <Link to="/Users" className="jety">Users </Link> */}
     <Link to="/Otp" className="jety">Chatbot </Link>
-    <Link to="/Community" className="jety">Community </Link>
+    <Link to="https://stackoverflowclonebyprashant.netlify.app/" className="jety">Community </Link>
     <Link to="/Plans" className="jety">Plans </Link>
     
   </div>;
